9374eca198585c382611f48a7d5aad3e94bdaa19
Gustavo Paco <gustavopaco@gmail.com>
2023-01-05 20:06:13 -0300

    Criacao do projeto Spring Data AmigosCode

---
+++ src/test/java/com/pacoprojects/WebMvnSbAmigoscodeSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacoprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvnSbAmigoscodeSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
