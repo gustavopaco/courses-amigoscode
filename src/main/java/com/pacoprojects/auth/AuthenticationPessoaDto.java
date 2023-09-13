16478531d2dd3263d5a5849d239d0b3edcca852b
Gustavo Paco <gustavopaco@gmail.com>
2023-09-13 04:26:34 -0300

    Migrando commits de projeto Springboot e spring security v6

--- src/main/java/com/pacoprojects/auth/AuthenticationPessoaDto.java
+++
@@ -1,9 +0,0 @@
-package com.pacoprojects.auth;
-
-import java.io.Serializable;
-
-/**
- * A DTO for the {@link com.pacoprojects.model.Pessoa} entity
- */
-public record AuthenticationPessoaDto(String username, String password) implements Serializable {
-}
--- src/main/java/com/pacoprojects/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.pacoprojects.model;
-
-public enum Role {
-    PESSOA_FISICA,
-    ADMIN
-}
